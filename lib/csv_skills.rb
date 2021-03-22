9f6d22251cd55b40dbcecdf0fa73de1eefbe0197
Ana Carolina Ferreira <acosferreira@gmail.com>
2021-03-21 18:34:53 -0700

    change count method

--- lib/csv_skills.rb
+++ lib/csv_skills.rb
@@ -1,37 +1,39 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'bundler/setup'
 require 'csv_parse'
+
 Bundler.require(:default)
-ERRORFILE = 'error.csv'
-def parse_file(inputfile, outputfile)
-  csv = CsvParse.new
-  data = csv.read_file(inputfile)
-  domains = find_domains(data)
-  totals = count_domains(domains)
-  csv.write_file(totals, outputfile)
-rescue Errno::ENOENT => e
-  csv.write_file(ERRORFILE, { 'Error' => e.message })
-rescue StandardError => e
-  csv.write_file(ERRORFILE, { 'Error' => e.message })
-end
 
-def find_domains(row_value)
-  domains = []
-  byebug
-  row_value.each do |line|
-    line.each do |elem|
-      domains << elem.split('@').last if elem.include?('@')
-    end
+# class CsvSkills
+  
+  ERRORFILE = 'error.csv'
+  HEADER = %w[domain count]
+  
+  def parse_file(inputfile, outputfile)
+    csv = CsvParse.new
+    data = csv.read_file(inputfile)
+    domains = find_domains(data)
+    totals = count_domains(domains)
+    csv.write_file(totals, outputfile, HEADER)
+  rescue Errno::ENOENT => e
+    csv.write_file(ERRORFILE, { 'Error' => e.message })
+  rescue StandardError => e
+    csv.write_file(ERRORFILE, { 'Error' => e.message })
   end
-  domains
-end
 
-def count_domains(domains)
-  Hash[
-    domains.group_by do |domain|
-      domain
-    end.map do |key, value|
-      [key, value.size]
+  def find_domains(row_value)
+    domains = []
+    row_value.each do |line|
+      line.each do |elem|
+        domains << elem.split('@').last if elem.include?('@')
+      end
     end
-  ]
-end
+    domains
+  end
+
+  def count_domains(domains)
+    domains.group_by{|domain| domain}.map {|key,value| [key, value.length]}
+  end
+ # end
