5ef97b33d3c0dba32fe0c26bc5354990dbd0c7e7
Ana Carolina Ferreira <acosferreira@gmail.com>
2021-03-21 20:59:42 -0700

    adjust context and remove repetition

--- spec/lib/csv_skills_spec.rb
+++ spec/lib/csv_skills_spec.rb
@@ -2,46 +2,48 @@
 
 require 'csv_skills'
 
-RSpec.describe 'csv_skills' do
-  context 'Read file and export result' do
-    subject { parse_file(file, output) }
+RSpec.describe 'CsvSkills' do
+  
+  context '#parse_file' do
+    subject { CsvSkills.parse_file(file, output) }
     let(:file) { 'spec/fixtures/roster.csv' }
     let(:output) { 'output.csv' }
     let(:result) { [['yahoo.com', 331], ['gmail.com', 330], ['hotmail.com', 339]] }
-    it 'generates a count of the domains used in email addresses' do
+    it 'return parse result' do
       expect(subject).to eq(result)
     end
   end
 
-  context 'Find domains' do
-    subject { find_domains(str_array) }
-    let(:str_array) { [['test', 'test@test.com', '1', 'm'], ['test@gmail.com', '']] }
-    it 'find all domains' do
-      expect(subject).to eq(['test.com', 'gmail.com'])
-    end
+  context '#find_domains' do
+    subject { CsvSkills.find_domains(row) }
+    context 'find domains' do 
+	    let(:row) { ['test', 'test@test.com', '1', 'm'] }
+	    it 'return test.com' do
+	      expect(subject).to eq('test.com')
+	    end
+  	end
+	  context 'domain not found' do
+	    let(:row) { %w[test 1 m] }
+	    it 'return empty result' do
+	      expect(subject).to be_nil
+	    end
+	  end
   end
 
-  context 'Domains not found' do
-    subject { find_domains(str_array) }
-    let(:str_array) { [%w[test 1 m], %w[test2 2]] }
-    it 'empty result' do
-      expect(subject).to eq([])
-    end
-  end
+  context '#count' do
+    subject { CsvSkills.count(value) }
+    context 'return count' do
+	    let(:value) { ['test.com', 'gmail.com'] }
+	   it 'return count test and gmail' do
+	      expect(subject).to eq([['test.com', 1], ['gmail.com', 1]])
+	    end
+	  end
 
-  context 'Count' do
-    subject { count_domains(str_array) }
-    let(:str_array) { ['test.com', 'gmail.com'] }
-    it 'count the domains on hash' do
-      expect(subject).to eq([["test.com", 1], ["gmail.com", 1]])
-    end
-  end
-
-  context 'Empty result' do
-    subject { count_domains(str_array) }
-    let(:str_array) { [] }
-    it 'should empty hash' do
-      expect(subject).to eq([])
-    end
-  end
+	  context 'domains not found' do
+	    let(:value) { [] }
+	    it 'return empty result' do
+	      expect(subject).to eq([])
+	    end
+	  end
+	end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'rspec'
+require 'byebug'
+
+RSpec.configure do |config|
+  config.color = true
+end
